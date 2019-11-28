5f61e42556cfa3c2c34f766b5f9fbaccc56e4c3d
Steffen Küllmer <30567862+nullsibnelf@users.noreply.github.com>
2019-11-28 09:25:33 +0100

    Update fiddle.ino

--- arduino_root/fiddle.ino
+++ arduino_root/fiddle.ino
@@ -1,1 +1,34 @@
 
+const int LED1 = 13;                // LED indicator
+const int analog_in = 0;            // Pin A0
+
+
+
+void setup()
+{
+  pinMode(analog_in, INPUT);        // Poti
+  pinMode(LED1, OUTPUT);            // Arduino Onboard-Led
+}
+
+
+
+int value get_midi (void)
+{
+   value = analogRead(analog_in);   // Read Poti value
+}
+
+
+
+void loop()
+{
+  
+  get_midi();
+
+
+
+
+
+
+  
+
+}
